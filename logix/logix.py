ba6afbd39fd429520aa3dd40947c44b1692b775f
Axel Salazar <118502945+Axelslz@users.noreply.github.com>
2023-11-06 20:28:01 -0600

    codigo corregido con la nueva gramatica

--- logix/logix.py
+++ logix/logix.py
@@ -1,8 +1,8 @@
 import tkinter as tk
 import re
 
-class validador:
-    def __init__(self, root):
+class validar:
+    def _init_(self, root):
         self.root = root
         self.root.title("Logixx")
         self.text_widget = tk.Text(root, wrap=tk.WORD, height=15, width=50)
@@ -20,26 +20,23 @@ class validador:
             self.result_label.config(text="Código inválido")
 
     def is_valid_code(self, code):
-        # Expresiones regulares para validar la declaración de variables y estructuras de funciones/sentencias
+        
         variable_pattern = r"(int|string|bool)\s+\$\w+\s*=\s*(\d+|\".*?\"|true|false);"
-        function_pattern = r"func\s+\w+\s*\(\s*(int|string|bool)\s+\$\w+\s*,\s*(int|string|bool)\s+\$\w+\s*\)\s*:\s*(int|string|bool)\s*{\s*retornar\s+\$\w+\s*[\+\-\/*]\s+\$\w+\s*;\s*}"
-        repeat_pattern = r"repeat\s+\(int\s+\$\w+\s*=\s*\d+;\s+\$\w+\s*(<|<=|>|>=|==|!=)\s*\d+;\s+\$\w+\s*\+\+|\$\w+\s*--\)\s*{\s*.*\s*}"
-        if_pattern = r"if\s+\(\s*\$\w+\s*(>|<)\s*\d+\)\s*{\s*.*\s*}\s*(else\s*{\s*.*\s*})?"
-        main_pattern = r"func\s+main\(\)\s*{\s*.*\s*}"
+        function_pattern = r"func\s+\w+\s*\(\s*(int|string|bool)\s+\$\w+\s*,\s*(int|string|bool)\s+\$\w+\s*\)\s*:\s*(int|string|bool)\s*{\s*retornar\s+\$\w+\s*[\+\-\/]\s+\$\w+\s;\s*}"
+        repeat_pattern = r"repeat\s+\(int\s+\$\w+\s*=\s*\d+;\s+\$\w+\s*(<|<=|>|>=|==|!=)\s*\d+;\s+\$\w+\s*\+\+|\$\w+\s*--\)\s*{\s*.\s}"
+        if_pattern = r"if\s+\(\s*\$\w+\s*(>|<)\s*\d+\)\s*{\s*.\s}\s*(else\s*{\s*.\s})?"
+        main_pattern = r"func\s+main\(\)\s*{\s*.\s}"
 
-        # Validar la declaración de variables, estructura de la función y sentencias
+        
         if re.match(variable_pattern, code) or re.match(function_pattern, code) or re.match(repeat_pattern, code) or re.match(if_pattern, code) or re.match(main_pattern, code):
-            # Verificar si falta alguna expresión en las declaraciones de variables
-            lines = code.strip().split('\n')
-            for line in lines:
-                if re.match(variable_pattern, line):
-                    if "=" not in line:
-                        return False
+            
+            if "$$" in code:  
+                return False
             return True
 
         return False
 
-if __name__ == "__main__":
+if __name__ == "_main_":
     root = tk.Tk()
-    ide_simulator = validador(root)
+    ide_simulator = validar(root)
     root.mainloop()
